30367818b1c2960b4c009aff5d95b8cfaa531685
root <faheemdev78@gmail.com>
2025-08-20 05:57:48 +0500

    remove unwanted

--- src-bk/app/profile/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-export default function ProfileHome() {
-    return (<div>
-        <h1>Profile Home</h1>
-    </div>)
-}
--- src-bk/lib/index.js
+++
@@ -1,4 +0,0 @@
-
-export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-export const isServer = typeof window === "undefined";
-
