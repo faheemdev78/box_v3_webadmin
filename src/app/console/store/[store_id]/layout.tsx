fb6d56dffa1ea754e52c9f58ec627f24ac4e874a
root <faheemdev78@gmail.com>
2025-12-10 22:48:33 +0500

    layout bug fixed

--- src/app/console/store/[store_id]/layout.tsx
+++ src/app/console/store/[store_id]/layout.tsx
@@ -4,11 +4,22 @@ import { PageBar } from '@_/template'
 import { createApolloClient } from '@_/aClient/client';
 import { getServerSessionToken } from '@_/lib/auth/server';
 
+
 import GET_STORE from '@_/graphql/stores/store.graphql';
 
-export default async function ConsoleLayout({ children, params }: { 
+
+// export default async function Layout(
+//     { children, params }: LayoutProps<'/console/store/[store_id]'>
+// ) {
+//     const { store_id } = params;
+//     return <div>{children}</div>;
+// }
+
+export default async function Layout({
+    children, params,
+}: {
     children: React.ReactNode;
-    params: { store_id: string };
+    params: Promise<{ store_id: string }>;
 }) {
     const { store_id } = await params;
     const baseUrl = `${adminRoot}/store/${store_id}`;
@@ -56,34 +67,3 @@ export default async function ConsoleLayout({ children, params }: {
     </>)
 }
 
-
-
-// 'use client'
-// import { adminRoot } from '@_/configs'
-// import { PageBar } from '@_/template'
-// import { useParams } from 'next/navigation'
-// import React from 'react'
-
-// export default function ConsoleLayout({ children }: { children: React.ReactNode }) {
-//     const { store_id } = useParams()
-//     const baseUrl = `${adminRoot}/store/${store_id}`
-
-//     return (<>
-//          <div className='page-bar'>
-//             <PageBar menuArray={[
-//                 { title: 'Baskets', href: `${baseUrl}/baskets` },
-//                 { title: 'Banners', href: `${baseUrl}/banners` },
-//                 // { title: 'Discount Vouchers', href: `${baseUrl}/discount_vouchers` },
-//                 // { title: 'Offers', href: `${baseUrl}/offers` },
-//                 // { title: 'Vendors', href: `${baseUrl}/vendors` },                
-//                 { title: 'Geo Zones', href: `${baseUrl}/zones` },
-//                 { title: 'Products', href: `${baseUrl}/products` },
-//                 { title: 'Orders', href: `${baseUrl}/orders` },
-//                 { title: 'Staff', href: `${baseUrl}/staff` },
-//                 { title: 'Vehicles', href: `${baseUrl}/vehicles` },
-//                 { title: 'Delivery Slots', href: `${baseUrl}/delivery_slots` },
-//             ]} />
-//         </div>
-//         {children}
-//     </>)
-// }
