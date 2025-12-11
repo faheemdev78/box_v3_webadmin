531b5a1edcf61fb46dd7822dd3a73460a579937c
root <faheemdev78@gmail.com>
2025-12-11 09:00:36 +0500

    debug

--- src/lib/utill_apollo.js
+++ src/lib/utill_apollo.js
@@ -4,6 +4,8 @@ import { __error } from "./consoleHelper";
     .then(r => checkApolloRequestErrors({ results: r, allowEmpty: true, parseReturn: (rr) => rr?.data?.user }))
 */
 export function checkApolloRequestErrors({ results, allowEmpty = false, parseReturn }){
+    console.log("results: ", results)
+    
     if (!results && allowEmpty) return results;
     if (!results) return { error: { message:"Invalid or empty results!" } }
 
