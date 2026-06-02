fd063601a47fc64822bb6ac122e2070746883fd5
root <faheemdev78@gmail.com>
2026-06-02 17:52:11 +0500

    updates

--- src/lib/fetchSettings.ts
+++ src/lib/fetchSettings.ts
@@ -8,7 +8,7 @@ import GET_CONFIGS from '@/graphql/settings/getSystemConfigs.graphql';
 
 
 export async function fetchSettings() {
-    // console.log("fetchSettings()")
+    console.log("fetchSettings()")
 
     const client = createApolloClient();
     console.log("client: ", client)
